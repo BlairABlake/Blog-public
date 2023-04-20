8d04b2fb306f7a144b66246d6eac78c352c51a5f
BlairABlake <k111ryousuke@gmail.com>
2023-04-21 08:06:29 +0900

    initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+import './plugins/search/mark.js'
+import './plugins/search/search.js'
+import './plugins/themify-icons/themify-icons.css'
+import $ from 'jquery';
+window.$ = window.jQuery = $;
+import 'slick-carousel';
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'venobox/src/venobox.esm.js'
+import 'fuse.js'
+import '../assets/js/script.js'
